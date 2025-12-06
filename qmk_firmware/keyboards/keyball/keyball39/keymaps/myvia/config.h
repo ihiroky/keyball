cd5cf9968cfad51783c6dbdfbe58b77da1deea9e
Hiroki Itoh <ihiroky@gmail.com>
2025-12-07 00:58:36 +0900

    feat: Add oled timer with manual toggle

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/myvia/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/myvia/config.h
@@ -27,6 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Prevent `quantum\keyboard.c` from calling oled_on().
 // Self housekeeping is required to handle oled timeout.
 #define OLED_TIMEOUT 0
+#define KEYBALL_OLED_TIMEOUT 10000
 
 #define TAP_CODE_DELAY 5
 
